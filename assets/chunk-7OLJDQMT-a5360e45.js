97159f4c4c230a02a0a4b5c7eaf63b5f4a4e88eb
vardhan-venkata <vardhanmerapureddy@gmail.com>
2024-01-01 12:59:16 +0530

    Updates

---
+++ assets/chunk-7OLJDQMT-a5360e45.js
@@ -0,0 +1,1 @@
+import{q as t,t as i,v as r,j as c,w as m,x as o}from"./index-e83be40c.js";var d=t(function(a,s){const e=i("Heading",a),{className:l,...n}=r(a);return c.jsx(m.h2,{ref:s,className:o("chakra-heading",a.className),...n,__css:e})});d.displayName="Heading";export{d as H};
